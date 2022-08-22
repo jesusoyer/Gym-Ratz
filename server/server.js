cd09b42939ab1fd4a4aeab73797161cbdc9b5172
jesusoyer <oyervidesjesus017@gmail.com>
2022-08-22 17:35:15 -0500

    added generic express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,25 @@
-//test
+const express = require('express');
+
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+
+
+  
+  
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    })
+
+ 
+  
+
