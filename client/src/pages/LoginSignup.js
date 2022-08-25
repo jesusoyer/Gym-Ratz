c16d9163c2925e6507dd472dd2ca26d3b006f7f9
thechicketarian <thechicketarian@gmail.com>
2022-08-25 14:02:25 -0500

    Restructuring Login/Sign Up  forms in components and pages. Wrote Auth function, needs connection to server and mutations in util

--- client/src/pages/LoginSignup.js
+++ client/src/pages/LoginSignup.js
@@ -1,8 +1,51 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-import backgroundVideo from '../images/vertheader.mp4'
-//need authentication import 
+import backgroundVideo from '../images/vertheader.mp4';
+import SignUp from '../components/SignUp'
 
+// import { Link } from 'react-router-dom';
+
+// import { useMutation } from '@apollo/client';
+// import { LOGIN_USER } from '../utils/mutations';
+
+// import Auth from '../utils/auth';
+
+// LOGIN JS
+//------------------------
+
+// const Login = (props) => {
+//     const [formState, setFormState] = useState({ email:'', password:''});
+//     const [login, {error, data}] = useMutation( LOGIN_USER);
+
+//     //update state based on from input changes
+
+//     const handleChange = (event) => {
+//         const {name, value } = event.target;
+//         serFormState({
+//             ...formState,
+//         [name]: value,
+//         });
+//     };
+
+// const handleFormSubmit = async (event) => {
+//     event.preventDefault();
+//     console.log(formState)
+//     try {
+//         const {data} = await login({
+//             variable: {...formState},
+//         });
+//         Auth.login(data.login.token);
+//     } catch (e) {
+//         console.error(e);
+//     }
+//     // clear form values
+//     setFormState({
+//         email: '',
+//         password: '',
+//       });
+// };
+ 
+
+//FAKE FORM FOR UI - can restructure when ready. LMK
 function LoginSignUp(){
 return (
 //react form goes here
@@ -11,7 +54,7 @@ return (
 
 <section className="mainFormContainer">
 
-<form className="loginForm">
+<form className="loginForm" >
 <h2> Login </h2>
     <label> Email: 
     <input className="form-input" placeholder="Email" name="email" type="email" value="" onChange=""/>
@@ -23,25 +66,13 @@ return (
 </form>
 
 <video className="videoHeader" autoPlay muted loop> <source src={backgroundVideo} type="video/mp4"/> </video>
-
-<form className="registerForm">
-<h2> Register </h2>
-    <label> Username: 
-    <input className="form-input" placeholder="username" name="username" type="text"  value="" onChange=""/>
-    </label>
-    <label> Email: 
-    <input className="form-input" placeholder="Email" name="email" type="email" value="" onChange=""/>
-    </label>
-    <label> Password: 
-    <input className="form-input" placeholder="*******" name="email" type="email" value="" onChange=""/>
-    </label>
-    <button style={{ cursor: 'pointer' }} type="submit"> Login </button>
-</form>
-
+<SignUp/>
 </section>
 </main>
 ) //end of return  
 }
+
+// } //end of login function
 
 //exports home page
 export default LoginSignUp;
