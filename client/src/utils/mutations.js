32624c9b19e8f117070579987e3a9b2a86166c98
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-26 17:30:12 -0500

    updated add workout mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,29 +1,45 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
+
+   mutation login($email: String!, $password: String!) {
+        login(email: $email, paswword: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+   }
+`;
+
+export const ADD_USER = gql`
+   mutation addUser($username: String!, $email: String!, $password: String!){
+        addUser(username: $username, email: $email, password:$password!) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+   }
 `;
 
 export const ADD_WORKOUT = gql`
-  mutation addWorkout($workout: WorkoutInput){
-    addWorkout(workout: $workout){
-      title
-      workoutAuthor
-      exercises {
-        title
-        sets
-        reps
-        weight
-        other
-      }
-    }
-  }
+   mutation addWorkout($workout: WorkoutInput) {
+        addWorkout(workout: $workout){
+            _id
+            workoutAuthor
+            title
+            exercises {
+                _id
+                title
+                sets
+                reps
+                weight
+                other
+            }
+        }
+   }
+
 `;
