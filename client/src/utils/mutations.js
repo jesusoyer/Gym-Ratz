598d324dcc12729544ad0573d5ff1b6d1983ebd0
jesusoyer <oyervidesjesus017@gmail.com>
2022-08-26 17:34:40 -0500

    changes

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -15,7 +15,7 @@ export const LOGIN_USER = gql`
 
 export const ADD_USER = gql`
    mutation addUser($username: String!, $email: String!, $password: String!){
-        addUser(username: $username, email: $email, password:$password!) {
+        addUser(username: $username, email: $email, password:$password) {
             token
             user {
                 _id
@@ -25,62 +25,39 @@ export const ADD_USER = gql`
    }
 `;
 
-export const ADD_WORKOUT = gql`
-   mutation addWorkout($workoutAuthor: String!, $title: String!) {
-        addWorkout(workoutAuthor: $workoutAuthor, title: $title){
-            _id
-            workoutAuthor
-            title
-            exercises {
-                -id
-                title
-            }
-            other
-        }
-   }
+// export const ADD_WORKOUT = gql`
+//    mutation addWorkout($workoutAuthor: String!, $title: String!) {
+//         addWorkout(workoutAuthor: $workoutAuthor, title: $title){
+//             _id
+//             workoutAuthor
+//             title
+//             exercises {
+//                 _id
+//                 title
+//             }
+//             other
+//         }
+//    }
 
-`;
+// `;
 
-export const ADD_EXERCISE = gql`
-   mutation addExercise($workoutId: ID!, $title: String!) {
-        addExercise(workoutId: $workoutId, title: $title) {
-            _id
-            workAuthor
-            title
-            exercises {
-                title
-                sets
-                reps
-                weight
-            }
-            other
-        }
-   }
+// export const ADD_EXERCISE = gql`
+//    mutation addExercise($workoutId: ID!, $title: String!) {
+//         addExercise(workoutId: $workoutId, title: $title) {
+//             _id
+//             workAuthor
+//             title
+//             exercises {
+//                 title
+//                 sets
+//                 reps
+//                 weight
+//             }
+//             other
+//         }
+//    }
 
-`;
+// `;
 
 
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
-`;
-<<<<<<< HEAD
-export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
-`;
-=======
->>>>>>> 5e40a93cf9ecad9beb8adf8fad96e4809e29c1d0
+
